7fdabbbdb0ef88ce7ede3b9db7658c6616e08fa4
sezanzeb <proxima@sezanzeb.de>
2021-03-05 12:16:40 +0100

    one more test

--- awxkit/test/cli/test_config.py
+++ awxkit/test/cli/test_config.py
@@ -69,6 +69,7 @@ def test_config_precedence():
     assert config.credentials.default.password == 'secret'
 
 def test_config_file_precedence():
+    """Ignores AWXKIT_CREDENTIAL_FILE if cli args are set"""
     os.mkdir('/tmp/awx-test/')
     with open('/tmp/awx-test/config.json') as f:
         json.dump({
@@ -93,7 +94,34 @@ def test_config_file_precedence():
     assert config.credentials.default.username == 'mary'
     assert config.credentials.default.password == 'secret'
 
+def test_config_file_precedence_2():
+    """Ignores AWXKIT_CREDENTIAL_FILE if TOWER_* vars are set."""
+    os.mkdir('/tmp/awx-test/')
+    with open('/tmp/awx-test/config.json') as f:
+        json.dump({
+            'default': {
+                'username': 'IGNORE',
+                'password': 'IGNORE'
+            }
+        }, f)
+
+    cli = CLI()
+    cli.parse_args(
+        ['awx'],
+        env={
+            'AWXKIT_CREDENTIAL_FILE': '/tmp/awx-test/config.json',
+            'TOWER_USERNAME': 'mary',
+            'TOWER_PASSWORD': 'secret'
+        }
+    )
+    with pytest.raises(ConnectionError):
+        cli.connect()
+
+    assert config.credentials.default.username == 'mary'
+    assert config.credentials.default.password == 'secret'
+
 def test_config_file():
+    """Reads username and password from AWXKIT_CREDENTIAL_FILE."""
     os.mkdir('/tmp/awx-test/')
     with open('/tmp/awx-test/config.json') as f:
         json.dump({
